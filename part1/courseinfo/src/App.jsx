7127b70e2309fb9ea4fe7972a3f7b4c23de0d696
diegojumagdao <diegojumagdao@gmail.com>
2026-06-18 06:08:43 +0100

    Destructured props in App.jsx

--- part1/courseinfo/src/App.jsx
+++ part1/courseinfo/src/App.jsx
@@ -1,37 +1,26 @@
-const Header = (props) => {
-  return <h1>{props.course.name}</h1>;
+const Header = ({ course }) => {
+  return <h1>{course.name}</h1>;
 };
 
-const Content = (props) => {
-  console.log(props);
-  return (
-    // <div>
-    // <Part name = {props.course.parts[0].name} exercises = {props.course.parts[0].exercises}/>
-    // <Part name = {props.course.parts[1].name} exercises = {props.course.parts[1].exercises}/>
-    // <Part name = {props.course.parts[2].name} exercises = {props.course.parts[2].exercises}/>
-    // </div>
-    props.course.parts.map((part) => (
-      <Part name={part.name} exercises={part.exercises} key={part.name} />
-    ))
-  );
+const Content = ({ course }) => {
+  console.log(course);
+  return course.parts.map((part) => (
+    <Part name={part.name} exercises={part.exercises} key={part.name} />
+  ));
 };
 
-const Total = (props) => {
-  const total = props.course.parts.reduce(
-    (sum, part) => sum + part.exercises,
-    0,
-  );
-  // return (
-  //   <p>Number of exercises is {props.course.parts[0].exercises + props.course.parts[1].exercises + props.course.parts[2].exercises}</p>
-  // )
+const Total = ({ course }) => {
+  console.log(course);
+  const total = course.parts.reduce((sum, part) => sum + part.exercises, 0);
+
   return <p>Number of exercises is {total}</p>;
 };
 
-const Part = (props) => {
-  console.log(props);
+const Part = ({ name, exercises }) => {
+  console.log(name, exercises);
   return (
     <p>
-      {props.name} {props.exercises}
+      {name} {exercises}
     </p>
   );
 };
